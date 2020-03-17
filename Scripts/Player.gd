15a73fa1124f09f0eb14dd304a4dc7cb96c388ea
Unknown <ohaiguyhai@Gmail.com>
2020-03-17 16:24:21 +1100

    Input using touch manager
    - much cleaner (hnng)
    - less state machine stuff
    - timer dictates going from dragging back to touching

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,72 +3,42 @@ extends RigidBody2D
 export (int) var drag_speed
 export (int) var hold_speed
 export (int) var maximum_drag_force
-export (int) var drag_speed_cutoff
 export (float) var drag_release_time
-var holding
 var dragging
-onready var timer = get_node("Timer")
+var holding_pos
+onready var timer_drag_release = get_node("Timer")
 
 func _ready():
 	InputManager.connect("single_touch", self, "touch")
 	InputManager.connect("single_drag", self, "drag")
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
-	timer.set_one_shot(true)
-	timer.set_wait_time(drag_release_time)
-	timer.connect("timeout", self, "release_timeout")
-
-func _input(event):
-	if event.is_action_pressed("click"):
-		start_holding()
-	elif event.is_action_released("click"):
-		stop_holding()
-	
-	if holding and event is InputEventMouseMotion:
-		dragging = event.get_speed().length() > drag_speed_cutoff
-		if dragging:
-			var vel = event.get_speed() / maximum_drag_force
-#			print("vel: ", event.get_speed().length())
-	#		print("Mouse Motion at: ", vel)
-			apply_central_impulse(vel * drag_speed)
+	timer_drag_release.set_one_shot(true)
+	timer_drag_release.set_wait_time(drag_release_time)
+	timer_drag_release.connect("timeout", self, "release_timeout")
 
 func _process(delta):
 	# print(linear_velocity)
 	rotate(linear_velocity.length() * delta)
 
 func _physics_process(delta):
-	if holding and not dragging:
-		# maybe store the viewport?
-		# impulse towards mouse point
-		var target = get_viewport().get_mouse_position()
-		var dir = (target - get_transform().origin).normalized()
+	if holding_pos != null and not dragging:
+		var dir = (holding_pos - get_transform().origin).normalized()
 		apply_central_impulse(dir * hold_speed)
-		
-		# look at mouse
-#		get_transform().get_origin().linear_interpolate(target,delta)
-#		target.lerp(get_transform().origin, 1)
-		# look_at(target)
-		#rotate(linear_velocity.length())
-		#print("holding!")
+
+func touch(event):
+	if (event.pressed):
+		hold_this(event.position)
 	else:
-		pass
-		#print("not!")
+		hold_this(null)
 
-func start_holding():
-	holding = true
+func drag(event):
+	apply_central_impulse(event.speed / maximum_drag_force * drag_speed)
+	dragging = true
+	hold_this(event.position)
+	timer_drag_release.start()
 
-func stop_holding():
-	holding = false
-	if dragging:
-		timer.start()
+func hold_this(pos):
+	holding_pos = pos
 
 func release_timeout():
 	dragging = false
 	# stop spinning
-
-func touch(val):
-	print('touching')
-	print(val)
-
-func drag(val):
-	print('dragging')
-	print(val)
