c20bd63d456c1f6fd8adc6c99632136914147813
Unknown <ohaiguyhai@Gmail.com>
2020-06-18 15:55:18 +1000

    Pickup particle effects
    - particle effects plays when you pick up a pickup

--- Scripts/Pickup.gd
+++ Scripts/Pickup.gd
@@ -3,9 +3,14 @@ extends Area2D
 var pickup_audio
 var ambient_audio
 var sprite
+var particles
 var picked_up
 var player
 var speed_mod
+
+var sound_done
+var particle_shooting
+
 export (float) var fly_speed
 onready var random_note = get_node("Randomiser")
 
@@ -13,7 +18,11 @@ func _ready():
 	picked_up = false
 	pickup_audio = get_node("Pickup Sound")
 	sprite = get_node("Sprite")
+	particles = get_node("Particle")
 	speed_mod = randf() + 0.5 # between 0.5 and 1.5
+	
+	sound_done = false
+	particle_shooting = false
 	
 	ambient_audio = get_node("Ambient Noise")
 	var ambient_randomiser = get_node("RandomiserAmbient")
@@ -29,10 +38,14 @@ func _on_Pickup_body_entered(body):
 		body.add_score()
 		sprite.hide()
 		play_sound()
+		particles.set_emitting(true)
+		particle_shooting = true
 		picked_up = true
 
 func _on_AudioStreamPlayer2D_finished():
-	# sound is done
+	sound_done = true
+
+func delete_self():
 	var parent = get_parent()
 	if parent.name == "Pickup Spawner":
 		parent.pickup_deleted(self)
@@ -47,6 +60,8 @@ func start_flying_to_player(player_obj):
 
 func _process(delta):
 	fly_to_player(delta)
+	if particle_shooting and sound_done and not particles.is_emitting():
+		delete_self()
 
 func fly_to_player(delta):
 	if player == null:
